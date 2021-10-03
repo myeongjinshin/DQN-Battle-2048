479db071ceff1ac2d5e03a429ab48b4d681ae5a2
Mingyu Lee <minkyu.lee65@gmail.com>
2021-10-03 22:30:07 +0900

    update load data

--- model/model.py
+++ model/model.py
@@ -3,23 +3,22 @@ from tensorflow import keras
 from keras.layers import Dense, Concatenate, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.optimizers import Adam
 
-
-def build_encoder():
-    model = keras.models.Sequential()
+def build_actor():
+    model = keras.models.Sequential();
     model.add(Conv2D(32, kernel_size=(3, 3), activation="relu", kernel_initializer="he_uniform"))
     model.add(Conv2D(32, kernel_size=(3, 3), activation="relu", kernel_initializer="he_uniform"))
     model.add(Flatten())
     model.add(Dense(128, activation="linear", kernel_initializer="he_uniform"))
-    return model
-
-def build_actor():
-    model = keras.models.Sequential();
     model.add(Dense(128, activation="relu", kernel_initializer="he_uniform"))
     model.add(Dense(4, activation="linear", kernel_initializer="he_uniform"))
     return model
 
 def build_critic():
     model = keras.models.Sequential();
+    model.add(Conv2D(32, kernel_size=(3, 3), activation="relu", kernel_initializer="he_uniform"))
+    model.add(Conv2D(32, kernel_size=(3, 3), activation="relu", kernel_initializer="he_uniform"))
+    model.add(Flatten())
+    model.add(Dense(128, activation="linear", kernel_initializer="he_uniform"))
     model.add(Dense(128, activation="relu", kernel_initializer="he_uniform"))
     model.add(Dense(1, activation="linear", kernel_initializer="he_uniform"))
     return model
